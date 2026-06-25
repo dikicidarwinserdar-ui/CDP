96ca2ed1d5d1e4e9ce2f28865f52d8cb6c24e112
dikicidarwinserdar-ui <dikicidarwinserdar@gmail.com>
2026-06-25 12:27:20 +0300

    Update analyzer.py

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,1 +1,17 @@
-a
+class CDPAnalyzer:
+
+    def __init__(self):
+
+        pass
+
+    def analyze(self, image):
+
+        return {
+
+            "decision": "UNKNOWN",
+
+            "score": 0.0,
+
+            "confidence": 0.0
+
+        }
